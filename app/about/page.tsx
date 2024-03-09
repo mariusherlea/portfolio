b264765fa166b35c9f26fa6686c57a202a6e6b6a
mariusherlea <mariusherlea@gmail.com>
2024-03-09 17:08:32 +0200

    add some metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Mh About',
+  description: 'About Mh',
+};
 
 const about = () => {
   return (
