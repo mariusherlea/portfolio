9ea5adc3066bcb01cf34143bbba9368daedd06eb
mariusherlea <mariusherlea@gmail.com>
2024-03-18 12:55:10 +0200

    loading and not-found pages

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1>Sorry</h1>
+      <p>This page is in progres...</p>
+    </div>
+  );
+}
