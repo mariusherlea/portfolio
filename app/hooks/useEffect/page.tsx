fa4c3f7d15bec7b5240c60a50a66a3fad64682fe
mariusherlea <mariusherlea@gmail.com>
2024-04-04 09:24:28 +0300

    rename resourceType

--- app/hooks/useEffect/page.tsx
+++ app/hooks/useEffect/page.tsx
@@ -3,35 +3,35 @@
 import React, { useState, useEffect } from 'react';
 
 const Page = () => {
-  const [resource, setResource] = useState('posts');
+  const [resourceType, setResourceType] = useState('posts');
   const [items, setItems] = useState([]);
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
       .then((res) => res.json())
       .then((json) => setItems(json));
-  }, [resourceUsage]);
+  }, [resourceType]);
 
   return (
     <>
       <div>
         <button
-          onClick={() => setResource('posts')}
+          onClick={() => setResourceType('posts')}
           className="m-4 bg-slate-500 hover:bg-red-200 rounded-xl p-3">
           Posts
         </button>
         <button
-          onClick={() => setResource('users')}
+          onClick={() => setResourceType('users')}
           className="m-4 bg-slate-500 hover:bg-red-200 rounded-xl p-3">
           Users
         </button>
         <button
-          onClick={() => setResource('comments')}
+          onClick={() => setResourceType('comments')}
           className="m-4 bg-slate-500 hover:bg-red-200 rounded-xl p-3">
           Coments
         </button>
       </div>
       <div>
-        <h1>{resource}</h1>
+        <h1>{resourceType}</h1>
         {items.map((item) => {
           return <pre key={item.id}>{JSON.stringify(item, null, 2)}</pre>;
         })}
